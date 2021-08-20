4ce6330969739a6b09e3396f71d9395c2bc73472
Lucas <lucas.pokemon.61@gmail.com>
2021-08-20 20:50:00 -0300

    First Commit

---
+++ venv/Calculador.py
@@ -0,0 +1,7 @@
+n1 = int(input('Me fale sua primeira nota: '))
+n2 = int(input('Me fale sua segunda nota: '))
+n3 = int(input('Me fale sua terceira nota: '))
+n4 = int(input('Me fale sua quarta nota: '))
+n5 = n1+n2+n3+n4
+n6 = n5/4
+print('Sua média é {}'.format(n6))
---
+++ venv/MiniProjeto01.py
@@ -0,0 +1,5 @@
+input('Suas notas foram 5, 7, 10 e 3')
+input('Estamos calculando sua média, aguarde alguns segundos....')
+n1 = 5+7+10+3
+n2 = n1/4
+print('A sua média é {}'.format(n2))
