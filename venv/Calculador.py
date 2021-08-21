a7563c2eccf9477f23285c638f5a8bdca7bffb55
Lucas <lucas.pokemon.61@gmail.com>
2021-08-21 18:58:08 -0300

    third commit

--- venv/Calculador.py
+++ venv/Calculador.py
@@ -1,7 +1,8 @@
-n1 = int(input('Me fale sua primeira nota: '))
-n2 = int(input('Me fale sua segunda nota: '))
-n3 = int(input('Me fale sua terceira nota: '))
-n4 = int(input('Me fale sua quarta nota: '))
+n1 = float(input('Me fale sua primeira nota: '))
+n2 = float(input('Me fale sua segunda nota: '))
+n3 = float(input('Me fale sua terceira nota: '))
+n4 = float(input('Me fale sua quarta nota: '))
 n5 = n1+n2+n3+n4
 n6 = n5/4
-print('Sua média é {}'.format(n6))
+print('Sua média é {:.2}'.format(n6))
+
